38365aa7612000813aa6676f788ad8e63c84757d
Steven <steven.povlitz@gmail.com>
2017-12-05 21:07:48 -0500

    you pushed to master with an error in it -'ian'

--- GrangerPovlitz_CombinationAccuracy.py
+++ GrangerPovlitz_CombinationAccuracy.py
@@ -65,7 +65,7 @@ def main():
 	# runs score and rank total for each combinations, and neatly prints out the result.
 	for attrs in combos:
 		c = rnk.determine_c(gazeData, attrs)
-		print attrs + ", c: " + str(c)
+		print str(attrs) + ", cutoff: " + str(c)
 		print "\tScore accuracy percentages: " + str(scr.score_total(gazeData, attrs, precisionAt))
 		print "\tRank accuracy percentages:  " + str(rnk.rank_total(gazeData, rankData, attrs, precisionAt, c))
 
